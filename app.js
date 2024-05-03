e0c571bf6ce870924fd34581ebd890d74951c098
Barış Dilekçi <barisdilekci@Bars-MacBook-Air.local>
2024-05-04 00:52:13 +0300

    feat: create workspace

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id: 1,
+    name: "Blog title",
+    description: "Blog description"
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
