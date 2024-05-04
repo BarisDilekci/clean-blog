281e381ac1c2a53444c694f1994a08905e84a7fd
Barış Dilekçi <barisdilekci@Bars-MacBook-Air.local>
2024-05-04 22:42:01 +0300

    feat: configure Template Engine

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const ejs = require("ejs");
+const express = require("express");
+
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.set("view engine", "ejs");
 
-  const photo = {
-    id: 1,
-    name: "Blog title",
-    description: "Blog description"
-  }
-  res.send(photo)
-})
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log(`Sunucu ${3000}'de baslatildi...`);
 });
