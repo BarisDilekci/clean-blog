3d9356ad6119502c62feb22558ed42ac9abf8e85
BarisDilekci <bdilekci200@gmail.com>
2024-05-08 02:42:25 +0300

    feat: create mongodb

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
+
 const ejs = require("ejs");
 const express = require("express");
-
+const Blog = require('./models/Blog');
+const mongoose = require('mongoose');
 const path = require("path");
-
 const app = express();
 
+mongoose.connect('mongodb://localhost/Blog', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB bağlantısı başarılı'))
+  .catch(err => console.error('MongoDB bağlantı hatası:', err));
+
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static("public"));
+const myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use(myLogger);
+
+app.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.render('index', { blogs });
+  } catch (err) {
+    console.error('Veritabanından blogları alma hatası:', err);
+    res.status(500).send('Veritabanından blogları alma hatası');
+  }
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
 app.get("/post", (req, res) => {
   res.render("post");
 });
+
 app.get("/add", (req, res) => {
   res.render("add");
 });
 
+app.post("/blogs", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { title, detail, author } = req.body;
+    if (!title || !detail) {
+      throw new Error('Başlık ve detay alanları zorunludur.');
+    }
+    await Blog.create({ title, detail, author });
+    res.redirect('/');
+  } catch (err) {
+    console.error('Blog oluşturma hatası:', err);
+    res.status(500).send('Blog oluşturma hatası');
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${3000}'de baslatildi...`);
+  console.log(`Sunucu ${port} üzerinde başlatıldı...`);
 });
---
+++ models/Blog.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+    title: String,
+    detail: String,
+    author: String,
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Blog = mongoose.model('Blog', BlogSchema);
+
+module.exports = Blog;
